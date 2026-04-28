1296e5496a81bea6b1fc741f007f7574ae001792
Chido <chido.efobi@gmail.com>
2026-04-28 14:26:39 -0500

    update message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello CI/CD World testApril282026_fflowers!"
+    return "Github version check"
 
 if __name__ == '__main__':
     app.run()
